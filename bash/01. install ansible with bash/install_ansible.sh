cbda099991c4739475eb4de9044e9e8e1f4649cd
Minhaz <minhaz1217@gmail.com>
2021-05-11 04:44:44 +0600

    deploying with docker from git with ansible working

--- bash/01. install ansible with bash/install_ansible.sh
+++ bash/01. install ansible with bash/install_ansible.sh
@@ -1,5 +1,6 @@
 sudo apt update
-sudo apt install software-properties-common
+sudo apt install software-properties-common -y
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible -y
+sudo apt install sshpass -y
 ansible --version
